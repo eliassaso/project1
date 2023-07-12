88e8d96636f7d9da8ab0ae92d6278fc0329bc41a
Elias <eliassaso@gmail.com>
2023-07-12 16:40:00 -0600

    Add project files.

---
+++ project1/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using project1.Models;
+using project1.Models.DAO;
+using project1.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.Web.Mvc;
+
+namespace project1.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserDAO userRepository = new UserDAO();
+        private AuthorizationConfig _db = new AuthorizationConfig();
+        // GET: User
+        public ActionResult Index()
+        {
+            
+            List<UserDTO> users = userRepository.ReadUsers();
+            // get an specific user (5)
+            UserDTO user = (from u in users where 5 == u.Id select u).First();
+            // get role
+            RolesUsuario ru = _db.RolesUsuario.Where(x => x.IdUser == user.Id).FirstOrDefault();
+            // asign a session variable  // role name
+            Session["role"] = _db.Roles.Where(x => x.Id == ru.IdRole).FirstOrDefault().Description;
+
+            return View(userRepository.ReadUsers());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserDTO user)
+        {
+            if (ModelState.IsValid) {
+                
+                return RedirectToAction("Index");
+
+            } else {
+                userRepository.InsertUser(user);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult getRole(int id)
+        {
+            try { 
+                int idRole = _db.RolesUsuario.Where(x => x.IdUser == id).FirstOrDefault().IdRole;
+                return Content(_db.Roles.Where(x => x.Id == idRole).FirstOrDefault().Description);
+            }
+            catch (Exception ex) {
+                return Content("DESCONOCIDO");
+            }
+        }
+    }
+}
